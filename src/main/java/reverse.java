b075db8514cfd86d68c53fc5f8311b96895a5231
QH_LEO <m15525835777@163.com>
2020-02-05 16:00:54 +0800

    Initial commit

---
+++ src/main/java/reverse.java
@@ -0,0 +1,18 @@
+/**
+ * @Author: Hao Qin
+ * @Date: 20-2-4  下午5:04
+ * @Version 1.0
+ */
+class reverse {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
---
+++ src/main/java/twosum.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * @Author: Hao Qin
+ * @Date: 20-2-4  下午3:56
+ * @Version 1.0
+ */
+public class twosum {
+
+    public static int[] call(int[] a, int b){
+        int l=a.length;
+        int aa [] = new int[l];
+        int i,j;
+        for(i=0;i<a.length-1;i++){
+            for(j=i+1;j<a.length;j++) {
+                if (a[i] + a[j] == b) {
+                    aa[0] = i;
+                    aa[1] = j;
+                }
+            }
+        }
+        return aa;
+    }
+    public static void main(String[] args) {
+
+        int[]num={7,11,8,1};
+        int target=9;
+        System.out.println(call(num,target)[0]);
+        System.out.println(call(num,target)[1]);
+    }
+}
