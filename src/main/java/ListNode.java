aff6d1d6910a9f3ff0b4c7b1c0087a3da2f89565
QH_LEO <m15525835777@163.com>
2020-02-05 21:22:09 +0800

    first commit

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,37 @@
+/**
+ * @Author: Hao Qin
+ * @Date: 20-2-5  下午5:49
+ * @Version 1.0
+ */
+
+
+  public class ListNode {
+     int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
+
